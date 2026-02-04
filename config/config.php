0ba70c4bf38072c0a6fceee40d1d9a12c73c1e28
Edmunds Priede <edmunds@kls.lv>
2026-02-04 10:40:56 +0200

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$config['name']        = 'Invoice Webhook';
+$config['description'] = 'Send invoice data to external webhook';
+$config['version']     = '1.0.0';
+$config['author']      = 'Ed & chat';
